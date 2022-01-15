edc41e5e1ca47a72742b341a3d9abebd867e8d92
RoboTuan <testingapplication96@gmail.com>
2022-01-15 12:53:52 +0100

    New example/experiment files

--- prova.py
+++ prova.py
@@ -2,24 +2,18 @@
 import torchreid
 import torch.nn as nn
 from torchreid.models import self_sup
-# import torch
-from torchreid.utils import set_random_seed, load_pretrained_weights, max_ham_permutations
+import torch
+from itertools import repeat
+from torchreid.utils import set_random_seed, load_pretrained_weights, resume_from_checkpoint
 # from torchreid.utils import load_pretrained_weights
 import sys
 from torchsummary import summary
+from collections import defaultdict, OrderedDict
 
 import numpy as np
 import random
 
 set_random_seed()
-# np.random.seed(10)
-# random.seed(10)
-
-# max_p = 31
-# P = max_ham_permutations(4, 2, max_p)
-# np.save("./perm_" + str(max_p), P)
-# sys.exit()
-
 
 # TODO:check the height and width that is different for each model (384x12, 256x128, ...)
 datamanager = torchreid.data.ImageDataManager(
@@ -28,139 +22,174 @@ datamanager = torchreid.data.ImageDataManager(
     targets='market1501',
     height=256,
     width=128,
-    batch_size_train=14,
+    batch_size_train=8,  # 14
     batch_size_test=100,
-    transforms=['random_flip', 'pad', 'random_crop'],
-    # transforms=['random_flip', 'pad', 'random_crop', 'random_erase'],
+    # transforms=None,
+    transforms=['random_flip', 'random_crop_translate'],
     val=False,
     combineall=True,
-    load_train_targets=True
-    # train_sampler='RandomIdentitySampler',
-    # num_instances=4
+    # workers=4,
+    load_train_targets=True,
+    norm_mean=[0.5] * 3,
+    norm_std=[0.5] * 3,
+    # train_sampler='RandomDatasetSampler',
+    # num_instances=2
+    n_samples=20  # taking only at max 20 images per identity for GTA_synthReid
 )
 # sys.exit()
-generators_name = 'generator'
-discriminator_S_name = 'discriminator_S'
-discriminator_R_name = 'discriminator_R'
 
-generator = torchreid.models.build_model(
+# backbone_name = 'metric_net'
+generator_S2R_name = 'generator_S2R'
+# generator_R2S_name = 'generator_R2S'
+# discriminator_S_name = 'discriminator_S'
+discriminator_R_name = 'discriminator_R'
+mlp_name = 'mlp'
+id_net_name = 'id_net'
+
+# backbone = torchreid.models.build_model(
+#     num_classes=datamanager.num_train_pids,
+#     name=backbone_name,
+#     adversarial=True,
+#     loss='triplet'
+# )
+
+generator_S2R = torchreid.models.build_model(
     num_classes=datamanager.num_train_pids,
-    name=generators_name,
+    name=generator_S2R_name,
     adversarial=True
 )
-discriminator_S = torchreid.models.build_model(
-    num_classes=datamanager.num_train_pids,
-    name=discriminator_S_name,
-    adversarial=True
-)
+
+# print(generator_S2R)
+# sys.exit()
+
+# generator_R2S = torchreid.models.build_model(
+#     num_classes=datamanager.num_train_pids,
+#     name=generator_R2S_name,
+#     adversarial=True
+# )
+
+# discriminator_S = torchreid.models.build_model(
+#     num_classes=datamanager.num_train_pids,
+#     name=discriminator_S_name,
+#     adversarial=True
+# )
+
 discriminator_R = torchreid.models.build_model(
     num_classes=datamanager.num_train_pids,
     name=discriminator_R_name,
     adversarial=True
 )
 
-generator.cuda()
-discriminator_S.cuda()
-discriminator_R.cuda()
+mlp = torchreid.models.build_model(
+    num_classes=datamanager.num_train_pids,
+    adversarial=True,
+    name=mlp_name,
+    use_mlp=True,
+    nc=256
+)
 
-# model = torchreid.models.build_model(
-#     name=model_name,
-#     num_classes=datamanager.num_train_pids,
-#     loss='softmax',
-#     # last_stride=2,
-#     self_sup=True,
-#     # neck='bnneck',
-#     # neck_feat='after',
-#     pretrained=True
+print(datamanager.num_train_pids)
+id_net = torchreid.models.build_model(
+    num_classes=datamanager.num_train_pids,
+    in_planes=256,
+    name=id_net_name,
+    adversarial=True,
+)
+
+# generator_S2R.cuda()
+# generator_R2S.cuda()
+# discriminator_S.cuda()
+# discriminator_R.cuda()
+
+# backbone = nn.DataParallel(backbone).cuda()
+generator_S2R = nn.DataParallel(generator_S2R).cuda()
+# generator_R2S = nn.DataParallel(generator_R2S).cuda()
+# discriminator_S = nn.DataParallel(discriminator_S).cuda()
+discriminator_R = nn.DataParallel(discriminator_R).cuda()
+mlp = nn.DataParallel(mlp).cuda()
+id_net = nn.DataParallel(id_net).cuda()
+
+# optimizer_B = torchreid.optim.build_optimizer(
+#     backbone_name,
+#     backbone,
+#     optim='adam',
+#     lr=2e-4,
+#     # weight_decay=5e-2
+#     # sgd_nesterov=True,
 # )
-# print(model)
-# for name, module in model.jig_saw_puzzle.named_children():
-#     print(name)
-#     for p in module.parameters():
-#         print(p.requires_grad)
-#     print()
 
-# sys.exit()
-
-# for data in datamanager.train_loader_t:
-#     print(data)
-#     break
-
-# for data in datamanager.train_loader:
-#     print(data)
-#     # model(data['img'])
-#     # print("Data")
-#     # print(data['img'].shape)
-#     break
-
-# sys.exit()
-
-# print(model)
-# model = nn.DataParallel(model)
-
-# model.classifier = nn.Sequential()
-# load_pretrained_weights(model, 'log/pcb_p6/model/model.pth.tar-10')
-
-# model = model.cuda()
-
-# summary(model, (3, 256, 128))
-# print(model)
-# sys.exit()
-
-new_layers_self_sup = [
-    'backbone.classifier',
-    # 'selfSup.feat_extractor',
-    # 'selfSup.global_avgpool',
-    # 'selfSup.flatten',
-    # 'selfSup.classifier'
-]
-
-optimizer_G = torchreid.optim.build_optimizer(
-    generators_name,
-    generator,
+optimizer_GS2R = torchreid.optim.build_optimizer(
+    generator_S2R_name,
+    generator_S2R,
     optim='adam',
+    adam_beta1=0.5,
     lr=2e-4,
     # sgd_nesterov=True,
 )
 
-optimizer_DS = torchreid.optim.build_optimizer(
-    discriminator_S_name,
-    discriminator_S,
-    optim='adam',
-    lr=2e-4,
-    # sgd_nesterov=True,
-)
+# optimizer_GR2S = torchreid.optim.build_optimizer(
+#     generator_R2S_name,
+#     generator_R2S,
+#     optim='adam',
+#     adam_beta1=0.5,
+#     lr=2e-4,
+#     # sgd_nesterov=True,
+# )
+
+# optimizer_DS = torchreid.optim.build_optimizer(
+#     discriminator_S_name,
+#     discriminator_S,
+#     optim='adam',
+#     adam_beta1=0.5,
+#     lr=2e-4,
+#     # sgd_nesterov=True,
+# )
 
 optimizer_DR = torchreid.optim.build_optimizer(
     discriminator_R_name,
     discriminator_R,
     optim='adam',
+    adam_beta1=0.5,
     lr=2e-4,
     # sgd_nesterov=True,
 )
 
-# scheduler = torchreid.optim.build_lr_scheduler(
-#     optimizer,
-#     lr_scheduler='warmup_multi_step',
-#     stepsize=[15, 17],
-#     warmup_iters=10,
-#     warmup_factor=1 / 10,
-#     warmup_method='linear',
+optimizer_id = torchreid.optim.build_optimizer(
+    id_net_name,
+    id_net,
+    optim='adam',
+    adam_beta1=0.5,
+    lr=2e-4,
+)
+
+
+# scheduler_B = torchreid.optim.build_lr_scheduler(
+#     optimizer_B,
+#     lr_scheduler='multi_step',
+#     stepsize=[15, 25],
+#     gamma=0.1
 # )
 
-scheduler_G = torchreid.optim.build_lr_scheduler(
-    optimizer_G,
+scheduler_GS2R = torchreid.optim.build_lr_scheduler(
+    optimizer_GS2R,
     lr_scheduler='multi_step',
     stepsize=[15, 25],
     gamma=0.1
 )
 
-scheduler_DS = torchreid.optim.build_lr_scheduler(
-    optimizer_DS,
-    lr_scheduler='multi_step',
-    stepsize=[15, 25],
-    gamma=0.1
-)
+# scheduler_GR2S = torchreid.optim.build_lr_scheduler(
+#     optimizer_GR2S,
+#     lr_scheduler='multi_step',
+#     stepsize=[15, 25],
+#     gamma=0.1
+# )
+
+# scheduler_DS = torchreid.optim.build_lr_scheduler(
+#     optimizer_DS,
+#     lr_scheduler='multi_step',
+#     stepsize=[15, 25],
+#     gamma=0.1
+# )
 
 scheduler_DR = torchreid.optim.build_lr_scheduler(
     optimizer_DR,
@@ -169,22 +198,58 @@ scheduler_DR = torchreid.optim.build_lr_scheduler(
     gamma=0.1
 )
 
-model_names = [generators_name, discriminator_S_name, discriminator_R_name]
+scheduler_id = torchreid.optim.build_lr_scheduler(
+    optimizer_id,
+    lr_scheduler='multi_step',
+    stepsize=[15, 25],
+    gamma=0.1
+)
+
+
+# model_names = [generator_S2R_name, generator_R2S_name, discriminator_S_name, discriminator_R_name]
+model_names = [id_net_name, generator_S2R_name, discriminator_R_name, mlp_name]
+
 models = {
-    'generator': generator,
-    'discriminator_S': discriminator_S,
+    # 'metric_net': backbone,
+    'generator_S2R': generator_S2R,
+    # 'generator_R2S': generator_R2S,
+    # 'discriminator_S': discriminator_S,
     'discriminator_R': discriminator_R,
+    'mlp': mlp,
+    'id_net': id_net
 }
+
 optimizers = {
-    'generator': optimizer_G,
-    'discriminator_S': optimizer_DS,
+    # 'metric_net': optimizer_B,
+    'generator_S2R': optimizer_GS2R,
+    # 'generator_R2S': optimizer_GR2S,
+    # 'discriminator_S': optimizer_DS,
     'discriminator_R': optimizer_DR,
+    'mlp': None,
+    'id_net': optimizer_id
 }
+
 schedulers = {
-    'generator': scheduler_G,
-    'discriminator_S': scheduler_DS,
+    # 'metric_net': scheduler_B,
+    'generator_S2R': scheduler_GS2R,
+    # 'generator_R2S': scheduler_GR2S,
+    # 'discriminator_S': scheduler_DS,
     'discriminator_R': scheduler_DR,
+    'mlp': None,
+    'id_net': scheduler_id
 }
+
+# for name in model_names:
+#     if name == 'mlp':
+#         dummy_feats = [torch.rand((1, 3)), torch.rand((1, 128))]
+#         dummy_feats.extend([torch.rand((1, 256))] * 3)
+#         mlp.module.create_mlp(dummy_feats)
+#         optimizer_mlp, scheduler_mlp = mlp.module.optim_sched()
+#         optimizers['mlp'] = optimizer_mlp
+#         schedulers['mlp'] = scheduler_mlp
+#     path = "log/CUT7/" + name + "/model.pth.tar-30"
+#     start_epoch = resume_from_checkpoint(path, models[name], optimizers[name], schedulers[name])
+#     print(start_epoch)
 
 engine = torchreid.engine.ImageAdversarialEngine(
     datamanager,
@@ -196,15 +261,17 @@ engine = torchreid.engine.ImageAdversarialEngine(
 )
 
 engine.run(
-    save_dir='log/gan',
-    max_epoch=4,
+    # start_epoch=start_epoch,
+    save_dir='log/CUT9',
+    max_epoch=8,
     eval_freq=6,
-    print_freq=10,
+    print_freq=250,
 )
 
 # load_pretrained_weights(model, 'log/im_resnet50_softmax_val_open[3_4_cls]_multi/model/model.pth.tar-30')
 # model = model.cuda()
 
 # engine.run(
-#     test_only=True
+#     test_only=True,
+#     rerank=True
 # )
