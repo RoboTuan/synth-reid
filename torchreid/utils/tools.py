77119a316220a2f892bed6c6487a7facd2c5a114
RoboTuan <testingapplication96@gmail.com>
2022-01-19 23:27:39 +0100

    New seed management

--- torchreid/utils/tools.py
+++ torchreid/utils/tools.py
@@ -63,8 +63,8 @@ def write_json(obj, fpath):
         json.dump(obj, f, indent=4, separators=(',', ': '))
 
 
-def set_random_seed():
-    seed = 10
+def set_random_seed(seed):
+    # seed = 10
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
